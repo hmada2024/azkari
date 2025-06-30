ba2cef70a5f201cfabe35a0db40f021e2f43282e
hmada2024 <alasuity881991@gmail.com>
2025-06-30 23:22:52 +0300

    1

--- lib/data/models/tasbih_model.dart
+++ lib/data/models/tasbih_model.dart
@@ -3,22 +3,27 @@
 class TasbihModel {
   final int id;
   final String text;
+  final String? alias;
   final int sortOrder;
   final bool isDeletable;
 
   TasbihModel({
     required this.id,
     required this.text,
+    this.alias,
     required this.sortOrder,
     required this.isDeletable,
   });
+
+  String get displayName => alias ?? text;
 
   factory TasbihModel.fromMap(Map<String, dynamic> map) {
     return TasbihModel(
       id: map['id'],
       text: map['text'],
+      alias: map['alias'],
       sortOrder: map['sort_order'],
-      isDeletable: map['is_deletable'] == 1, 
+      isDeletable: map['is_deletable'] == 1,
     );
   }
 }
