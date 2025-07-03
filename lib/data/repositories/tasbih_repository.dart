72c5df5e28e3c73452a849e4e27abcf1a9f7f884
hmada2024 <alasuity881991@gmail.com>
2025-07-03 06:10:50 +0300

    2

--- lib/data/repositories/tasbih_repository.dart
+++ lib/data/repositories/tasbih_repository.dart
@@ -5,12 +5,20 @@ import '../models/tasbih_model.dart';
 class TasbihRepository {
   final TasbihDao _tasbihDao;
   TasbihRepository(this._tasbihDao);
+
   Future<List<TasbihModel>> getCustomTasbihList() =>
       _tasbihDao.getCustomTasbihList();
+
+  Future<List<TasbihModel>> getActiveTasbihList() =>
+      _tasbihDao.getActiveTasbihList();
+
   Future<TasbihModel> addTasbih(String text) => _tasbihDao.addTasbih(text);
-  Future<void> deleteTasbih(int id) => _tasbihDao.deleteTasbih(id);
+
+  // Future<void> deleteTasbih(int id) => _tasbihDao.deleteTasbih(id); // <-- تم الحذف
+
   Future<void> updateTasbihText(int id, String newText) =>
       _tasbihDao.updateTasbihText(id, newText);
+
   Future<void> updateSortOrders(Map<int, int> newOrders) =>
       _tasbihDao.updateSortOrders(newOrders);
 }
